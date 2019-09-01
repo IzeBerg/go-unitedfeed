314c865640ec82975a546fc4697b8eaaa5d5822b
Renat Iliev <me@izeberg.ru>
2019-09-02 02:12:56 +0300

    albums forwarding

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ const redisChatsKey = "chats"
 var Debug = os.Getenv(`DEBUG`) != ``
 var ChatUsername = os.Getenv(`TG_USERNAME`)
 
+var chatsCache = map[string]*tdlib.Chat{}
+
 func init() {
 	tdlib.SetLogVerbosityLevel(1)
 	if err := sentry.Init(sentry.ClientOptions{Debug: Debug}); err != nil {
@@ -32,12 +34,10 @@ func initTG() *tdlib.Client {
 		if _, err := fmt.Scanln(&code); err == nil {
 			return code
 		} else {
-			sentry.CaptureException(err)
 			panic(err)
 		}
 	})
 	if err != nil {
-		sentry.CaptureException(err)
 		panic(err)
 	}
 	if state.GetAuthorizationStateEnum() != tdlib.AuthorizationStateReadyType {
@@ -51,40 +51,44 @@ func initDB() *redis.Client {
 	if db, err := GetRedis(os.Getenv(`REDIS_URL`)); err == nil {
 		return db
 	} else {
-		sentry.CaptureException(err)
 		panic(err)
 	}
 }
 
 func ForwardMessages(tg *tdlib.Client, username string, fromMessageID int64, to *tdlib.Chat) (int64, error) {
-	if chat, err := tg.SearchPublicChat(username); err == nil {
+	if from, err := GetChat(tg, username); err == nil {
 		if fromMessageID == 0 {
 			fromMessageID = 1
 		}
-		if messages, err := tg.GetChatHistory(chat.ID, fromMessageID, -99, 100, false); err == nil {
+		if messages, err := tg.GetChatHistory(from.ID, fromMessageID, -99, 100, false); err == nil {
 			if len(messages.Messages) > 0 {
 				sort.SliceStable(messages.Messages, func(i, j int) bool {
 					return messages.Messages[i].ID < messages.Messages[j].ID
 				})
 				var messageIDs []int64
+				var mediaAlbumID tdlib.JSONInt64
 				for _, msg := range messages.Messages {
 					if msg.CanBeForwarded && fromMessageID < msg.ID {
+						if len(messageIDs) == 0 {
+							mediaAlbumID = msg.MediaAlbumID
+						}
+						if msg.MediaAlbumID != mediaAlbumID {
+							break
+						}
 						messageIDs = append(messageIDs, msg.ID)
 					}
 				}
 				if len(messageIDs) > 0 {
-					log.Println(`fwd`, chat, `->`, to, messageIDs)
-					_, err := tg.ForwardMessages(to.ID, chat.ID, messageIDs, false, false, false)
+					log.Println(`fwd`, from.ID, `->`, to.ID, messageIDs, mediaAlbumID != 0)
+					_, err := tg.ForwardMessages(to.ID, from.ID, messageIDs, false, false, mediaAlbumID != 0)
 					if err == nil {
-						return messages.Messages[len(messages.Messages)-1].ID, nil
+						return messageIDs[len(messageIDs)-1], nil
 					} else {
 						return 0, err
 					}
 				}
 			}
 		} else {
-			log.Println(err, chat, fromMessageID)
-			SentryHub(map[string]interface{}{`chat`: chat, `offset`: fromMessageID}).CaptureException(err)
 			return 0, err
 		}
 	} else {
@@ -93,22 +97,40 @@ func ForwardMessages(tg *tdlib.Client, username string, fromMessageID int64, to 
 	return fromMessageID, nil
 }
 
-func Update(db *redis.Client, tg *tdlib.Client, chat *tdlib.Chat)  {
+func GetChat(tg *tdlib.Client, username string) (*tdlib.Chat, error) {
+	if chat, found := chatsCache[username]; found {
+		return chat, nil
+	}
+	if chat, err := tg.SearchPublicChat(username); err == nil {
+		chatsCache[username] = chat
+		return chat, err
+	} else {
+		return nil, err
+	}
+}
+
+func Update(db *redis.Client, tg *tdlib.Client, chat *tdlib.Chat) {
+	for username, fromMessageID := range GetChats(db) {
+		if newFromMessageID, err := ForwardMessages(tg, username, fromMessageID, chat); err == nil {
+			if newFromMessageID != fromMessageID {
+				if err := db.HSet(redisChatsKey, username, strconv.FormatInt(newFromMessageID, 10)).Err(); err != nil {
+					log.Println(err, username, fromMessageID, newFromMessageID)
+					SentryHub(map[string]interface{}{`key`: username, `value`: fromMessageID, `new`: newFromMessageID}).CaptureException(err)
+				}
+			}
+		} else {
+			log.Println(err, username, fromMessageID)
+			SentryHub(map[string]interface{}{`key`: username, `value`: fromMessageID}).CaptureException(err)
+		}
+	}
+}
+
+func GetChats(db *redis.Client) map[string]int64 {
+	res := map[string]int64{}
 	if data, err := db.HGetAll(redisChatsKey).Result(); err == nil {
 		for username, fromMessageIDStr := range data {
 			if fromMessageID, err := strconv.ParseInt(fromMessageIDStr, 10, 64); err == nil {
-				if newFromMessageID, err := ForwardMessages(tg, username, fromMessageID, chat); err == nil {
-					if newFromMessageID != 0 && newFromMessageID != fromMessageID {
-						newFromMessageIDStr := strconv.FormatInt(newFromMessageID, 10)
-						if err := db.HSet(redisChatsKey, username, newFromMessageIDStr).Err(); err != nil {
-							log.Println(err, username, fromMessageIDStr, newFromMessageIDStr)
-							SentryHub(map[string]interface{}{`key`: username, `value`: fromMessageIDStr, `new`: newFromMessageIDStr}).CaptureException(err)
-						}
-					}
-				} else {
-					log.Println(err, username, fromMessageIDStr)
-					SentryHub(map[string]interface{}{`key`: username, `value`: fromMessageIDStr}).CaptureException(err)
-				}
+				res[username] = fromMessageID
 			} else {
 				log.Println(err, username, fromMessageIDStr)
 				SentryHub(map[string]interface{}{`key`: username, `value`: fromMessageIDStr}).CaptureException(err)
@@ -117,6 +139,7 @@ func Update(db *redis.Client, tg *tdlib.Client, chat *tdlib.Chat)  {
 	} else {
 		panic(err)
 	}
+	return res
 }
 
 func main() {
@@ -146,14 +169,13 @@ func main() {
 		finiFileDatabase(db, redisTdlibKey, tdlibDatabaseDir)
 	}()
 
-	chat, err := tg.SearchPublicChat(ChatUsername)
+	chat, err := GetChat(tg, ChatUsername)
 	if err != nil {
 		panic(err)
 	}
 
 	for {
-		time.Sleep(time.Minute)
 		Update(db, tg, chat)
+		time.Sleep(time.Minute)
 	}
-
 }
